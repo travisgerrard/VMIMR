3c62e21f430a0686c2acb0aca1213f47e7628e9d
Travis Gerrard <travisgerrard@gmail.com>
2017-12-31 00:12:14 -0800

    Fixed landing page problem

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import jwt_decode from 'jwt-decode';
 
-const Landing = () => {
-  return (
-    <h5>{`Hi there ${jwt_decode(localStorage.getItem('token')).name}`}</h5>
-  );
-};
+class Landing extends Component {
+  renderLandingPage = () => {
+    if (localStorage.getItem('token') !== null) {
+      return (
+        <h5>{`Hi there ${jwt_decode(localStorage.getItem('token')).name}`}</h5>
+      );
+    }
+    return <h5>Login to get started</h5>;
+  };
+
+  render() {
+    return <div>{this.renderLandingPage()}</div>;
+  }
+}
 
 export default Landing;
