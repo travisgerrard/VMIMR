b7335d44d6aff431f4ab473fc4b969a6b8ed48f6
Travis Gerrard <travisgerrard@gmail.com>
2018-04-19 09:13:07 -0700

    Guts of adding moved to funciton

--- client/src/components/conditionGraphQl/AddCondition.js
+++ client/src/components/conditionGraphQl/AddCondition.js
@@ -44,8 +44,88 @@ class AddCondition extends Component {
     this.props.cancelAddingcondition();
   };
 
-  gutsOfAddLearning = (options, listOfUsers) => {
-    return <div />;
+  gutsOfAddLearning = (options, listOfUsers, addLearning) => {
+    return (
+      <div>
+        <Segment.Group stacked>
+          <Segment>
+            <Input
+              label="Name"
+              placeholder="Ex: ARDS"
+              value={this.state.conditionTitle}
+              onChange={(params, data) =>
+                this.setState({ conditionTitle: data.value })
+              }
+            />
+            <Select
+              options={options}
+              search
+              multiple
+              placeholder="Rotation tags"
+              onChange={(params, data) => this.setState({ tags: data.value })}
+            />
+          </Segment>
+          <Segment>
+            <Input
+              label="Attending"
+              placeholder="Ex: Baliga"
+              value={this.state.attending}
+              onChange={(params, data) =>
+                this.setState({ attending: data.value })
+              }
+            />
+            <Input
+              label="Date"
+              placeholder=""
+              value={this.state.date}
+              onChange={(params, data) => this.setState({ date: data.value })}
+            />
+            <Select
+              options={listOfUsers}
+              search
+              multiple
+              placeholder="Learned With"
+              onChange={(params, data) =>
+                this.setState({ userTags: data.value })
+              }
+            />
+            <Form>
+              <TextArea
+                placeholder="What was learned"
+                value={this.state.wwl}
+                onChange={(params, data) => this.setState({ wwl: data.value })}
+              />
+            </Form>
+          </Segment>
+          <Segment>
+            <div className="ui two buttons">
+              <Button
+                basic
+                color="green"
+                onClick={() => {
+                  addLearning({
+                    variables: {
+                      condition: this.state.conditionTitle,
+                      tags: this.state.tags,
+                      attending: this.state.attending,
+                      date: this.state.date,
+                      userTags: this.state.userTags,
+                      wwl: this.state.wwl,
+                    },
+                  });
+                  this.props.doneAddingLearning();
+                }}
+              >
+                Save
+              </Button>
+              <Button basic color="grey" onClick={() => this.cancelClicked()}>
+                Cancel
+              </Button>
+            </div>
+          </Segment>
+        </Segment.Group>
+      </div>
+    );
   };
 
   render() {
@@ -71,93 +151,7 @@ class AddCondition extends Component {
               >
                 {(addLearning, { data }) => (
                   <div>
-                    <Segment.Group stacked>
-                      <Segment>
-                        <Input
-                          label="Name"
-                          placeholder="Ex: ARDS"
-                          value={this.state.conditionTitle}
-                          onChange={(params, data) =>
-                            this.setState({ conditionTitle: data.value })
-                          }
-                        />
-                        <Select
-                          options={options}
-                          search
-                          multiple
-                          placeholder="Rotation tags"
-                          onChange={(params, data) =>
-                            this.setState({ tags: data.value })
-                          }
-                        />
-                      </Segment>
-                      <Segment>
-                        <Input
-                          label="Attending"
-                          placeholder="Ex: Baliga"
-                          value={this.state.attending}
-                          onChange={(params, data) =>
-                            this.setState({ attending: data.value })
-                          }
-                        />
-                        <Input
-                          label="Date"
-                          placeholder=""
-                          value={this.state.date}
-                          onChange={(params, data) =>
-                            this.setState({ date: data.value })
-                          }
-                        />
-                        <Select
-                          options={listOfUsers}
-                          search
-                          multiple
-                          placeholder="Learned With"
-                          onChange={(params, data) =>
-                            this.setState({ userTags: data.value })
-                          }
-                        />
-                        <Form>
-                          <TextArea
-                            placeholder="What was learned"
-                            value={this.state.wwl}
-                            onChange={(params, data) =>
-                              this.setState({ wwl: data.value })
-                            }
-                          />
-                        </Form>
-                      </Segment>{' '}
-                      <Segment>
-                        <div className="ui two buttons">
-                          <Button
-                            basic
-                            color="green"
-                            onClick={() => {
-                              addLearning({
-                                variables: {
-                                  condition: this.state.conditionTitle,
-                                  tags: this.state.tags,
-                                  attending: this.state.attending,
-                                  date: this.state.date,
-                                  userTags: this.state.userTags,
-                                  wwl: this.state.wwl,
-                                },
-                              });
-                              this.props.doneAddingLearning();
-                            }}
-                          >
-                            Save
-                          </Button>
-                          <Button
-                            basic
-                            color="grey"
-                            onClick={() => this.cancelClicked()}
-                          >
-                            Cancel
-                          </Button>
-                        </div>
-                      </Segment>
-                    </Segment.Group>
+                    {this.gutsOfAddLearning(options, listOfUsers, addLearning)}
                   </div>
                 )}
               </Mutation>
