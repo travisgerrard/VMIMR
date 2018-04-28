65e84381016ff72a7e50903f81670425a353f10b
Travis Gerrard <travisgerrard@gmail.com>
2018-04-27 22:59:27 -0400

    Refarctor add provider mutation

--- client/src/components/rotations/RotationProviders.js
+++ client/src/components/rotations/RotationProviders.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { withApollo, graphql } from 'react-apollo';
-import { Segment, Button, Form } from 'semantic-ui-react';
+import { Mutation } from 'react-apollo';
+import { Segment, Button, Form, Grid } from 'semantic-ui-react';
 
 import ADD_PROVIDER from '../../mutations/AddProvider';
 import SELECTED_ROTATION from '../../queries/SelectedRotation';
@@ -16,51 +16,68 @@ class RotationProviders extends Component {
   handleAddProviderClick = e => {
     this.setState({ addProvider: true });
   };
-  handleAddProviderSubmit = e => {
-    this.props
-      .mutate({
-        variables: {
-          name: this.state.providerName,
-          associatedRotation: this.props.id,
-          generalInfo: this.state.providerGeneralInfo,
-          _creator: this.props.creator,
-        },
-        refetchQueries: [
-          { query: SELECTED_ROTATION, variables: { id: this.props.id } },
-        ],
-      })
-      .then(this.setState({ addProvider: false }))
-      .catch(res => {
-        const errors = res.graphQLErrors.map(error => error.message);
-        this.setState({ errors });
-      });
-  };
 
   adminProviderInfo = () => {
     if (this.state.addProvider) {
       return (
-        <Form onSubmit={() => this.handleAddProviderSubmit()}>
-          <Form.Input
-            fluid
-            label="Provider Name"
-            value={this.state.providerName}
-            onChange={e => this.setState({ providerName: e.target.value })}
-          />
-          <Form.TextArea
-            label="Provider Info"
-            value={this.state.providerGeneralInfo}
-            onChange={e =>
-              this.setState({ providerGeneralInfo: e.target.value })
-            }
-          />
-          <Form.Button>Submit</Form.Button>
-        </Form>
+        <Mutation
+          mutation={ADD_PROVIDER}
+          refetchQueries={[
+            { query: SELECTED_ROTATION, variables: { id: this.props.id } },
+          ]}
+          onCompleted={() => {
+            this.setState({
+              addProvider: false,
+              providerName: '',
+              providerGeneralInfo: '',
+            });
+          }}
+        >
+          {(addProvider, { data }) => (
+            <Form
+              onSubmit={() =>
+                addProvider({
+                  variables: {
+                    name: this.state.providerName,
+                    associatedRotation: this.props.id,
+                    generalInfo: this.state.providerGeneralInfo,
+                    _creator: this.props.creator,
+                  },
+                })
+              }
+            >
+              <Form.Input
+                fluid
+                label="Provider Name"
+                value={this.state.providerName}
+                onChange={e => this.setState({ providerName: e.target.value })}
+              />
+              <Form.TextArea
+                label="Provider Info"
+                value={this.state.providerGeneralInfo}
+                onChange={e =>
+                  this.setState({ providerGeneralInfo: e.target.value })
+                }
+              />
+              <Form.Group>
+                <Form.Button>Submit</Form.Button>
+                <Form.Button
+                  onClick={() => this.setState({ addProvider: false })}
+                >
+                  Cancel
+                </Form.Button>
+              </Form.Group>
+            </Form>
+          )}
+        </Mutation>
       );
     } else {
       return (
-        <Button size="mini" onClick={() => this.handleAddProviderClick()}>
-          Add
-        </Button>
+        <div>
+          <Button size="mini" onClick={() => this.handleAddProviderClick()}>
+            Add
+          </Button>
+        </div>
       );
     }
   };
@@ -75,7 +92,26 @@ class RotationProviders extends Component {
             {providers.map(provider => {
               return (
                 <Segment key={provider.id}>
-                  <h4>{provider.name}</h4>
+                  <Grid>
+                    <Grid.Column width={8}>
+                      <h4>{provider.name}</h4>
+                    </Grid.Column>
+                    <Grid.Column width={4}>
+                      <Button
+                        size="mini"
+                        onClick={() => console.log('edit clicked')}
+                      >
+                        Edit
+                      </Button>
+                      <Button
+                        size="mini"
+                        color="red"
+                        onClick={() => console.log('delete clicked')}
+                      >
+                        Delete
+                      </Button>
+                    </Grid.Column>
+                  </Grid>
                   <p>{provider.generalInfo}</p>
                 </Segment>
               );
@@ -110,4 +146,4 @@ class RotationProviders extends Component {
   }
 }
 
-export default withApollo(graphql(ADD_PROVIDER)(RotationProviders));
+export default RotationProviders;
