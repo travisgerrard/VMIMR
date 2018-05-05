e67fc86b1329ddb2a6450d49ae5b0824da29c1d5
Travis Gerrard <travisgerrard@gmail.com>
2018-05-05 15:51:41 -0700

    Beginnigs of a calander

--- client/src/components/landing/LoggedInLanding.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class LoggedInLanding extends Component {
-  render() {
-    return <div style={{ marginTop: '4.5em' }}>You're logged in</div>;
-  }
-}
-
-export default LoggedInLanding;
