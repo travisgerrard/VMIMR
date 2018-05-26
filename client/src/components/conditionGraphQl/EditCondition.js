21a934a61b2142cf19f68794ff8c04082171cbd2
Travis Gerrard <travisgerrard@gmail.com>
2018-05-26 00:26:51 -0700

    fixed deleting ability

--- client/src/components/conditionGraphQl/EditCondition.js
+++ client/src/components/conditionGraphQl/EditCondition.js
@@ -1,7 +1,7 @@
 // Edit condition form
 
 import React, { Component } from 'react';
-import { Segment, Form, Loader, Message, Confirm } from 'semantic-ui-react';
+import { Segment, Form, Loader, Message } from 'semantic-ui-react';
 import _ from 'lodash';
 import { Query, Mutation } from 'react-apollo';
 import rotations from '../conditions/rotations';
@@ -144,26 +144,21 @@ class EditCondition extends Component {
                 fluid
                 color="red"
                 onClick={() => {
-                  this.setState({ confirmOpen: true });
+                  if (
+                    window.confirm(
+                      'Are you sure you want to save this thing into the database?',
+                    )
+                  ) {
+                    deleteLearning({
+                      variables: {
+                        id: this.props.learning.id,
+                      },
+                    });
+                  }
                 }}
               >
                 Delete
               </Form.Button>
-              <Confirm
-                open={this.state.confirmOpen}
-                content={`Are you sure you want to delete this learning, "${
-                  this.state.conditionTitle
-                }"?`}
-                onCancel={() => this.setState({ confirmOpen: false })}
-                onConfirm={() => {
-                  deleteLearning({
-                    variables: {
-                      id: this.props.learning.id,
-                    },
-                  });
-                  this.setState({ confirmOpen: false });
-                }}
-              />
             </Form.Group>
           </Form>
         </Segment>
