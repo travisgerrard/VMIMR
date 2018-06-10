dee04f4a6da79b2a2c0280d2fb8bedf0cad0483a
Travis Gerrard <travisgerrard@gmail.com>
2018-06-10 13:30:30 -0700

    Fiex bug...

--- client/src/components/conditionGraphQl/ContitionTopLevelView.js
+++ client/src/components/conditionGraphQl/ContitionTopLevelView.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { Container, Loader, Message, Modal } from 'semantic-ui-react';
 import { Query, withApollo } from 'react-apollo';
 import _ from 'lodash';
+import jwt_decode from 'jwt-decode';
 
 import GET_ALL_LEARNING from '../../queries/ListOfAllLearning';
 import GET_PERSONAL_LEARNING from '../../queries/ListOfPersonalLearning';
@@ -154,6 +155,55 @@ class ConditionTopLevelViewGQL extends Component {
     );
   };
 
+  displayLearning = () => {
+    const currentUser = jwt_decode(localStorage.getItem('VMIMRToken'));
+    if (this.state.sortActiveItem === 'Your Personal Learning') {
+      return (
+        <Query
+          query={GET_PERSONAL_LEARNING}
+          variables={{ id: currentUser.id }}
+          fetchPolicy="network-only"
+        >
+          {({ loading, error, data }) => {
+            if (loading) return <Loader active inline="centered" />;
+            if (error) return `Error! ${error.message}`;
+
+            console.log(data);
+
+            const filteredQuery = this.filterQuery(data.listOfPersonalLearning);
+
+            return (
+              <div>
+                {!data.listOfPersonalLearning.length && (
+                  <Message
+                    positive
+                    content={`It looks like you have not created or been tagged in any learning yet. Click "All Learning" above to show all the learning that has been created by others users.`}
+                  />
+                )}
+                {this.isAddingCondition(filteredQuery, currentUser)}
+              </div>
+            );
+          }}
+        </Query>
+      );
+    } else {
+      return (
+        <Query query={GET_ALL_LEARNING} fetchPolicy="network-only">
+          {({ loading, error, data }) => {
+            if (loading) return <Loader active inline="centered" />;
+            if (error) return `Error! ${error.message}`;
+
+            const filteredQuery = this.filterQuery(data.listOfAllLearning);
+
+            return (
+              <div>{this.isAddingCondition(filteredQuery, currentUser)}</div>
+            );
+          }}
+        </Query>
+      );
+    }
+  };
+
   render() {
     return (
       <Container style={{ marginTop: 10 }}>
@@ -161,75 +211,7 @@ class ConditionTopLevelViewGQL extends Component {
           handleItemClick={this.handleSortItemClick}
           activeItem={this.state.sortActiveItem}
         />
-
-        <Query query={GET_CURRENT_USER}>
-          {({ loading, error, data }) => {
-            if (loading) return 'Loading...';
-            if (error) return `Error! ${error.message}`;
-
-            if (this.state.sortActiveItem === 'Your Personal Learning') {
-              return (
-                <Query
-                  query={GET_PERSONAL_LEARNING}
-                  variables={{ id: data.currentUser.id }}
-                  fetchPolicy="network-only"
-                >
-                  {({ loading, error, data }) => {
-                    if (loading) return <Loader active inline="centered" />;
-                    if (error) return `Error! ${error.message}`;
-
-                    const filteredQuery = this.filterQuery(
-                      data.listOfPersonalLearning,
-                    );
-                    const currentUserQuery = this.props.client.readQuery({
-                      query: GET_CURRENT_USER,
-                    });
-
-                    return (
-                      <div>
-                        {!data.listOfPersonalLearning.length && (
-                          <Message
-                            positive
-                            content={`It looks like you have not created or been tagged in any learning yet. Click "All Learning" above to show all the learning that has been created by others users.`}
-                          />
-                        )}
-                        {this.isAddingCondition(
-                          filteredQuery,
-                          currentUserQuery.currentUser,
-                        )}
-                      </div>
-                    );
-                  }}
-                </Query>
-              );
-            } else {
-              return (
-                <Query query={GET_ALL_LEARNING} fetchPolicy="network-only">
-                  {({ loading, error, data }) => {
-                    if (loading) return <Loader active inline="centered" />;
-                    if (error) return `Error! ${error.message}`;
-
-                    const filteredQuery = this.filterQuery(
-                      data.listOfAllLearning,
-                    );
-                    const currentUserQuery = this.props.client.readQuery({
-                      query: GET_CURRENT_USER,
-                    });
-
-                    return (
-                      <div>
-                        {this.isAddingCondition(
-                          filteredQuery,
-                          currentUserQuery.currentUser,
-                        )}
-                      </div>
-                    );
-                  }}
-                </Query>
-              );
-            }
-          }}
-        </Query>
+        {this.displayLearning()}
       </Container>
     );
   }
